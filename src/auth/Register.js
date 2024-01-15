99680de1500a8dd3e03bf394167c19567f2dd1de
Alexlegard <alexlegard3@gmail.com>
2024-01-14 22:15:57 -0500

    Jan 14
    Jan 14 Commit

--- src/auth/Register.js
+++ src/auth/Register.js
@@ -1,13 +1,15 @@
 import { React, useRef, useState, useEffect } from "react";
 import { faCheck, faTimes, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import './Register.css';
+import './Auth.css';
+import axios from './api/axios';
 
 // Username must be one letter followed by one number, letter, dash or underscore.
 const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{3,23}$/;
 // Password must be 8-24 characters and must include one lowercase, one uppercase,
 // one number, and one special character.
 const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}/;
+const REGISTER_URL = '/register';
 
 function Register() {
     // These refs are useful to put the focus on various things.
@@ -60,13 +62,51 @@ function Register() {
         setErrMsg('');
     }, [user, pwd, matchPwd])
 
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        // If button enabled by hacking user
+        const usernameValid = USER_REGEX.test(user);
+        const passwordValid = PWD_REGEX.test(pwd);
+        if (!usernameValid || !passwordValid) {
+            setErrMsg("Invalid Submission");
+            return;
+        }
+        setSuccess(true);
+        /*
+        try {
+            const response = await axios.post(REGISTER_URL,
+                JSON.stringify({ user, pwd }),
+                {
+                    headers: { 'content-type': 'application/json' },
+                    withCredentials: true
+                }
+            );
+            console.log(response.data);
+            console.log(response.accessToken);
+            console.log(JSON.stringify(response));
+            setSuccess(true);
+            // TODO Clear input fields
+        } catch (err) {
+            if (!err?.response) {
+                setErrMsg('No Server Response');
+            } else if (err.response?.status === 409) {
+                setErrMsg('Username taken.');
+            } else {
+                setErrMsg('Registration Failed');
+            }
+            errRef.current.focus();
+
+        }*/
+
+    }
+
     return (
         <section className="registration-card">
             <div className="register-header">
                 <h1>Register</h1>
             </div>
 
-            <form className="registration-form">
+            <form onSubmit={handleSubmit} className="registration-form">
                 <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"}
                     aria-live="assertive">{errMsg}</p>
                 <label htmlFor="username">
